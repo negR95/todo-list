f6d3cc4e89e19f67e81e9b0da7a0cd51768562d0
negR95 <negar.mahdavi95@gmsil.com>
2022-06-12 13:53:49 +0430

    other change

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let toDoContainer = document.getElementById("toDoContainer");
 let inputField = document.getElementById("inputField");
 
 addToDoButton.addEventListener("click", function () {
-  var paragraph = document.createElement("p");
+  const paragraph = document.createElement("p");
   paragraph.classList.add("paragraph-styling");
   paragraph.innerText = inputField.value;
   toDoContainer.appendChild(paragraph);
